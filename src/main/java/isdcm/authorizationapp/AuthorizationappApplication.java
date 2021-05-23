0a075f0033b6d3ce2e630e5d36bb065bcf76dab0
Miguel Antunez <miguel.antunez@upcnet.es>
2021-05-23 20:10:16 +0200

    Initial commit

---
+++ src/main/java/isdcm/authorizationapp/AuthorizationappApplication.java
@@ -0,0 +1,13 @@
+package isdcm.authorizationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/isdcm/authorizationapp/AuthorizationappApplicationTests.java
@@ -0,0 +1,13 @@
+package isdcm.authorizationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
